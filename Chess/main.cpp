8110692b2e533ed4a9a07467e29096376f514742
jdbriner07 <jdbriner07@gmail.com>
2018-04-23 17:31:23 -0700

    Initial Commit

---
+++ Chess/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Chess
+//
+//  Created by Jeffrey Briner on 4/23/18.
+//  Copyright © 2018 Jeffrey Briner. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
